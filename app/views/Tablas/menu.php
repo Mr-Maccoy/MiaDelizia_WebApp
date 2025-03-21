bf76ab966fe54991ad45d6bd5c978a540a743cb4
Brayton Maccoy <134474843+Mr-Maccoy@users.noreply.github.com>
2025-03-20 22:44:02 -0600

    Mejoras esteticas

---
+++ app/views/Tablas/menu.php
@@ -0,0 +1,50 @@
+
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="index.php">Mia Delizia</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+
+
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Tablas
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="Tablas/clientes.php">Tabla Cliente</a>
+            <a class="dropdown-item" href="Tablas/pedidos.php">Tabla Pedidos</a>
+            <a class="dropdown-item" href="Tablas/eventos.php">Tabla Eventos</a>
+            <a class="dropdown-item" href="Tablas/inventario.php">Tabla Inventario</a>
+            <a class="dropdown-item" href="Tablas/productos.php">Tabla Productos</a>
+            <a class="dropdown-item" href="Tablas/categorias.php">Tabla Categorias</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="Tablas/facturas.php">Tabla Facturas</a>
+            <a class="dropdown-item" href="Tablas/roles.php">Tabla Roles</a>
+            <a class="dropdown-item" href="Tablas/usuarios.php">Tabla Usuarios</a>
+        </div>
+          </li>
+
+
+        
+        </ul>
+        
+      </div>
+    </nav>
+
+    <a href="Tablas/clientes.php">Tabla Cliente</a>
+<a href="Tablas/pedidos.php">Tabla Pedidos</a>
+<a href="Tablas/eventos.php">Tabla Eventos</a>
+<a href="Tablas/inventario.php">Tabla Inventario</a>
+<a href="Tablas/productos.php">Tabla Productos</a>
+<a href="Tablas/categorias.php">Tabla Categorias</a>
+
+<a href="Tablas/facturas.php">Tabla Facturas</a>
+<a href="Tablas/roles.php">Tabla Roles</a>
+<a href="Tablas/usuarios.php">Tabla Usuarios</a>
